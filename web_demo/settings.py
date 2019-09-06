96bb18551d1c5370cc7c2ccf5255d85e57908a33
ShravaN <Shravan2967@gmail.com>
2019-09-06 13:23:35 +0530

    Changes-- * Added Postgres

--- web_demo/settings.py
+++ web_demo/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
