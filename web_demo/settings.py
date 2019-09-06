018173580acedd4b34bcc33c696da70769c6e38a
ShravaN <Shravan2967@gmail.com>
2019-09-06 07:03:10 +0530

    Changes-- * Adding Static files

--- web_demo/settings.py
+++ web_demo/settings.py
@@ -154,6 +154,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
